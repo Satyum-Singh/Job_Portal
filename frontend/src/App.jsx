c27f02076bc5477474a9af38cbeeab44204f0f8d
Satyum <satyumspathania@gmail.com>
2024-08-28 20:47:26 +0530

    setting up files for frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-red-500 text-xl font-semibold'>Lets Build Job Portal</h1>
+    </>
+  )
+}
+
+export default App
