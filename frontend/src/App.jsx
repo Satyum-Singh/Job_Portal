64106a97d23a48337bd6eda56fb882d88def0523
SaloniThakur123 <salonithakur.0109@gmail.com>
2024-08-29 20:58:07 +0530

    msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,79 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  // admin ke liye yha se start hoga
+  {
+    path: "/admin/companies",
+    element: (
+      <ProtectedRoute>
+        <Companies />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/companies/create",
+    element: (
+      <ProtectedRoute>
+        <CompanyCreate />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/companies/:id",
+    element: (
+      <ProtectedRoute>
+        <CompanySetup />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs",
+    element: (
+      <ProtectedRoute>
+        <AdminJobs />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs/create",
+    element: (
+      <ProtectedRoute>
+        <PostJob />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs/:id/applicants",
+    element: (
+      <ProtectedRoute>
+        <Applicants />
+      </ProtectedRoute>
+    ),
+  },
+]);
 function App() {
   return (
-    <>
-      <h1 className='text-red-500 text-xl font-semibold'>Lets Build Job Portal</h1>
-    </>
-  )
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
 }
 
-export default App
+export default App;
